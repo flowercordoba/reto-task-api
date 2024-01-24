d51b68d6bfa678e0b8c8adcd8bad00f4b3df0f34
flowercordoba <flowercordoba7@gmail.com>
2024-01-24 12:00:11 -0500

    added channel discort

--- src/routes.ts
+++ src/routes.ts
@@ -7,12 +7,14 @@ import { Application } from 'express';
 import { notificacionRoutes } from './feature/notificacion/routes/notification.routes';
 import { AuthMiddleware } from '@middleware/auth-middleware';
 import { userRoutes } from '@user/routes/userRoutes';
+import { GitRoutes } from './feature/github/routes';
 
 const BASE_PATH = '/api';
 
 export default (app: Application) => {
   const routes = () => {
     app.use(BASE_PATH, authRoutes);
+    app.use(BASE_PATH, GitRoutes.routes);
     app.use(BASE_PATH, AuthMiddleware.validateJWT, currentUserRoutes.routes());
     app.use(BASE_PATH, AuthMiddleware.validateJWT, notificacionRoutes.routes());
     app.use(BASE_PATH, AuthMiddleware.validateJWT, taskRoutes.routes());
